3be2a49cc7303037c5ba3e4c0cb52324dd4ec40c
Nitish-Bakshi-1 <nitishbakshi05@gmail.com>
2025-01-04 07:34:54 +0530

    routes

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,17 @@
 import express from "express";
 import mongoose from "mongoose";
 import jwt from "jsonwebtoken";
+
+const app = express();
+
+app.post("api/v1/signup", function (req, res) {});
+
+app.post("api/v1/signin", function (req, res) {});
+
+app.post("api/v1/content", function (req, res) {});
+
+app.get("api/v1/content", function (req, res) {});
+
+app.delete("api/v1/content", function (req, res) {});
+
+app.post("api/v1/brain/share", function (req, res) {});
