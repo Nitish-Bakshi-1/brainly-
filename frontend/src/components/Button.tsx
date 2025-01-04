2e5839e6dff377c2b034f141bd5e1a3700aca4ad
Nitish-Bakshi-1 <nitishbakshi05@gmail.com>
2025-01-04 16:23:16 +0530

    init

---
+++ frontend/src/components/Button.tsx
@@ -0,0 +1,21 @@
+import { ReactElement } from "react";
+
+export interface ButtonProps {
+  variant: "primary" | "secondary";
+  size: "sm" | "md" | "lg";
+  text: string;
+  startIcon?: ReactElement;
+  endIcon?: ReactElement;
+  onClick: () => void;
+}
+
+const variantStyles = {
+  primary: "bg-purple-600 text-white",
+  secondary: "bg-purple-300 text-purple-500",
+};
+
+const Button = (props: ButtonProps) => {
+  return <button className={variantStyles[props.variant]}></button>;
+};
+
+export default Button;
