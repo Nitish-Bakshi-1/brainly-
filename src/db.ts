e62ae8bfc955b3b6014c42e4c1d7850f469b4418
Nitish-Bakshi-1 <nitishbakshi05@gmail.com>
2025-01-04 10:07:45 +0530

    init

--- src/db.ts
+++ src/db.ts
@@ -1,14 +1,22 @@
-import mongoose, { Model, Schema } from "mongoose";
+import mongoose, { model, Schema } from "mongoose";
 
-mongoose.connect(
-  "mongodb+srv://admin:yX6EbhfQXdv9ueZk@cluster0.j7jusce.mongodb.net/second-brain"
-);
+try {
+  mongoose
+    .connect(
+      "mongodb+srv://admin:yX6EbhfQXdv9ueZk@cluster0.j7jusce.mongodb.net/second-brain"
+    )
+    .then(() => {
+      console.log("database connected");
+    });
+} catch (e) {
+  console.log(e);
+}
 
 const userSchema = new Schema({
   username: { type: String, required: true, unique: true },
   password: { type: String, required: true },
 });
 
-const User = new Model("User", userSchema);
+const User = model("User", userSchema);
 
 export { User };
--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,47 @@
 import express from "express";
-import mongoose from "mongoose";
-import jwt from "jsonwebtoken";
+import { User } from "./db";
 
 const app = express();
+app.use(express.json());
 
-app.post("api/v1/signup", function (req, res) {});
+app.post("/api/v1/signup", async function (req, res) {
+  try {
+    const { username, password } = req.body;
+    await User.create({
+      username,
+      password,
+    });
+    res.json({
+      msg: "user signed up successfully",
+    });
+  } catch (e) {
+    console.log(e);
+  }
+});
 
-app.post("api/v1/signin", function (req, res) {});
+app.post("/api/v1/signin", async function (req, res) {
+  const { username, password } = req.body;
+  const user = await User.findOne({
+    username,
+    password,
+  });
+  if (!user) {
+    res.json({
+      msg: "user doesnot exists",
+    });
+  }
+});
 
-app.post("api/v1/content", function (req, res) {});
+app.post("/api/v1/content", function (req, res) {});
 
-app.get("api/v1/content", function (req, res) {});
+app.get("/api/v1/content", function (req, res) {});
 
-app.delete("api/v1/content", function (req, res) {});
+app.delete("/api/v1/content", function (req, res) {});
 
-app.post("api/v1/brain/share", function (req, res) {});
+app.post("/api/v1/brain/share", function (req, res) {});
 
-app.post("api/v1/brain/:shareLink", function (req, res) {});
+app.post("/api/v1/brain/:shareLink", function (req, res) {});
+
+app.listen(3000, () => {
+  console.log("server connected ");
+});
