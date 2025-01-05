d055f8d8e2d0a35bcdc10ade0ff2981dd4a6f097
Nitish-Bakshi-1 <nitishbakshi05@gmail.com>
2025-01-06 04:46:21 +0530

    init

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,5 @@
 import Button from "./components/Button";
-import Card from "./components/Card";
+import { Card } from "./components/Card";
 import PlusIcon from "./icons/PlusIcon";
 import ShareIcon from "./icons/ShareIcon";
 
