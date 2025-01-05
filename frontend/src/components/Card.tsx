aae72b81edcaf5472a6fac2a5db18781ac7986b9
Nitish-Bakshi-1 <nitishbakshi05@gmail.com>
2025-01-06 05:04:46 +0530

    init

--- frontend/src/components/Card.tsx
+++ frontend/src/components/Card.tsx
@@ -20,6 +20,17 @@ const Card = () => {
             </div>
           </div>
         </div>
+        <div className="pt-4">
+          <iframe
+            className="w-full"
+            src="https://www.youtube.com/embed/W4pCpqTBtyY?si=yh0ISUR1664R0PVm"
+            title="YouTube video player"
+            frameBorder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+            referrerPolicy="strict-origin-when-cross-origin"
+            allowFullScreen
+          ></iframe>
+        </div>
       </div>
     </div>
   );
