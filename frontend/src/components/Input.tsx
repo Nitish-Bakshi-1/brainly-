fd87fd25fb4b99de795319a84f2eab05d4c96105
Nitish-Bakshi-1 <nitishbakshi05@gmail.com>
2025-01-06 09:35:24 +0530

    init

--- frontend/src/components/Input.tsx
+++ frontend/src/components/Input.tsx
@@ -1,16 +1,17 @@
 function Input({
-  onChange,
   placeHolder,
+  ref,
 }: {
   placeHolder: string;
+  ref: any;
   onChange: () => void;
 }) {
   return (
     <input
       className="px-4 py-2 border rounded m-2"
       placeholder={placeHolder}
-      onChange={onChange}
       type="text"
+      ref={ref}
     />
   );
 }
--- frontend/src/pages/Signup.tsx
+++ frontend/src/pages/Signup.tsx
@@ -1,13 +1,33 @@
+import { useRef } from "react";
 import Button from "../components/Button";
 import Input from "../components/Input";
+import axios from "axios";
+import { BACKEND_URL } from "../config";
 
 const Signup = () => {
+  const usernameRef = useRef<HTMLInputElement>();
+  const passowrdRef = useRef<HTMLInputElement>();
+
+  async function signUp() {
+    const username = usernameRef.current?.value;
+    const password = passowrdRef.current?.value;
+    await axios.post(`${BACKEND_URL}/api/vi/signup`, {
+      username,
+      password,
+    });
+  }
+
   return (
     <div className="h-screen w-full bg-gray-200 flex justify-center  items-center ">
       <div className="bg-white border min-w-48 flex justify-center px-12 rounded-xl gap-4 py-8 flex-col items-center">
-        <Input placeHolder="username" />
-        <Input placeHolder="password" />
-        <Button text="Signup" variant="primary" fullWidth={true} />
+        <Input ref={usernameRef} placeHolder="username" />
+        <Input ref={passowrdRef} placeHolder="password" />
+        <Button
+          text="Signup"
+          variant="primary"
+          onClick={signUp}
+          fullWidth={true}
+        />
       </div>
     </div>
   );
