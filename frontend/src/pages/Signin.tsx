01aa84afb1446f142725c925a2ef5136eddf1359
Nitish-Bakshi-1 <nitishbakshi05@gmail.com>
2025-01-06 10:30:21 +0530

    init

--- frontend/src/pages/Signin.tsx
+++ frontend/src/pages/Signin.tsx
@@ -1,13 +1,35 @@
+import { useRef } from "react";
 import Button from "../components/Button";
 import Input from "../components/Input";
+import { BACKEND_URL } from "../config";
+import axios from "axios";
 
 const SignIn = () => {
+  const usernameRef = useRef<HTMLInputElement>();
+  const passwordRef = useRef<HTMLInputElement>();
+
+  async function signIn() {
+    const username = usernameRef.current?.value;
+    const password = passwordRef.current?.value;
+    const response = await axios.post(`${BACKEND_URL}/api/v1/signin`, {
+      username,
+      password,
+    });
+    const jwt = response.data.token;
+    localStorage.setItem("token", jwt);
+  }
+
   return (
     <div className="h-screen w-full bg-gray-200 flex justify-center  items-center ">
       <div className="bg-white border min-w-48 flex justify-center px-12 rounded-xl gap-4 py-8 flex-col items-center">
-        <Input placeHolder="username" />
-        <Input placeHolder="password" />
-        <Button text="SignIn" variant="primary" fullWidth={true} />
+        <Input reference={usernameRef} placeHolder="username" />
+        <Input reference={passwordRef} placeHolder="password" />
+        <Button
+          text="SignIn"
+          onClick={signIn}
+          variant="primary"
+          fullWidth={true}
+        />
       </div>
     </div>
   );
